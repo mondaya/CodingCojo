13e297f2dc18a49ef1ab67abd3122e4b0aa6c494
Monday Ayewa <monday.ayewa80.gmail.com>
2017-02-18 12:15:06 -0800

    remove myEnvironment form repo

--- pythonstack/myEnvironments/djangoEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mone\desktop\coding~1\python~1\myenvi~1\django~1\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
